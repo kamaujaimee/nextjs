3458d2bc6eba111a7f7af0555a3ab5aac52b978b
James Kamau <jkamau@netguardians.co.ke>
2024-09-26 11:16:46 +0300

    Update Response

--- pages/api/lexis.js
+++ pages/api/lexis.js
@@ -97,11 +97,6 @@ let items = {
                                     "ID": 0,
                                     "Type": "DOB",
                                     "Value": "1984-05-XX"
-                                },
-                                {
-                                    "ID": 0,
-                                    "Type": "Position",
-                                    "Value": "Charged with conspiracy and fraud - November 2021."
                                 }
                             ],
                             "Addresses": [
